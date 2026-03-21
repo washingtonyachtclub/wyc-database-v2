91b2f9ab1f0e63223b13720cdb2910fe4b616677
Eshan Arora <eshansarora@gmail.com>
2026-03-21 11:28:43 -0700

    email template tweak

--- src/lib/membership-processing/new-member-email-template.ts
+++ src/lib/membership-processing/new-member-email-template.ts
@@ -17,7 +17,7 @@ The password is unique and you can reset it in the database.
 
 Want to start learning immediately? Check our guides: washingtonyachtclub.org/guides
 
-And in case you don't have it already here is our discord: https://discord.gg/JRQECaeYKN
+And here is our discord in case you don't have it already: https://discord.gg/JRQECaeYKN
 It is definitely the best way to keep up with everything the club is up to.
 
 If you have any questions feel free to ask in discord!
